e178e66db2fec7145f32fb5a1d730147d04aa03f
bagel <freetgm@gmail.com>
2017-12-09 16:38:21 +0800

    update

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+<p>{{ test }}</p>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data: function () {
+    return {
+      test: 'hello world! I am a test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 26px;
+}
+</style>
